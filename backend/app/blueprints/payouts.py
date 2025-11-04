7a3e437faee66d9cdb9c0051dbab06955f8dd6e2
Donald Banda <donalddbanda@gmail.com>
2025-11-05 00:03:33 +0200

    Refactor code structure for improved readability and maintainability

---
+++ backend/app/blueprints/payouts.py
@@ -0,0 +1,13 @@
+from app import db
+from app.models import Payouts
+from .auth import company_or_admin_required
+from flask import request, jsonify, abort, Blueprint, current_app
+
+
+payouts_bp = Blueprint('payoutts', __name__)
+
+@payouts_bp.route('/payouts/request', method=["POST"])
+@company_or_admin_required
+def request_payout():
+    """ Request payouts to company """
+    pass
